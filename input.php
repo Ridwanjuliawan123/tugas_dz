434653e19332bfdffb081797223d033046e8141a
Ridwanjuliawan123 <113905013+Ridwanjuliawan123@users.noreply.github.com>
2023-10-05 09:47:06 +0700

    Add files via upload

---
+++ input.php
@@ -0,0 +1,35 @@
+<?php
+
+$nilai = 101;
+
+if($nilai > 100){
+
+echo("NU BALEG");
+
+}else if ($nilai >= 90){
+
+echo("BAGUS BANGET");
+
+}else if ($nilai >= 80){
+     echo ("BAGUS AJA");
+
+}else if ($nilai >= 70){
+     echo ("BAGUS BAGUS");
+
+}else if ($nilai >= 60){
+     echo ("BAGUS");
+
+}else if ($nilai >= 50){
+     echo ("CUKUP");
+
+}else if ($nilai >= 40){
+    echo("SANGAT JELEK");
+
+}else if ($nilai >= 30){
+echo("JELEK BANGET"); }
+
+else if ($nilai >= 0){
+echo ("GATAU");
+}
+
+?>
